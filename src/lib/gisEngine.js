e01b2a3746d13cb227fd052eb265c65ca6e1fc55
Iqbal Ghazi <iqbalghazi6998711@gmail.com>
2026-07-09 02:43:45 +0000

    File changes

--- src/lib/gisEngine.js
+++ src/lib/gisEngine.js
@@ -996,7 +996,7 @@ export function buildMapHeaderText(mapData) {
   const zilladarSection = mapData?.zilladar_section || "_____";
   const tehsil = mapData?.tehsil || "_____";
   const district = mapData?.district || "_____";
-  const mogaToken = `${side ? `${side}/` : ""}${number}`;
+  const mogaToken = `${number}${side ? `/${side}` : ""}`;
   return `خاکہ دستی موگہ نمبری\u2066${mogaToken}\u2069، راجباہ ${rajbah}،موضع ${village}، ضلعداری سیکشن ${zilladarSection}، سب ڈویژن ${tehsil}، ڈویژن ${district}`;
 }
 
